8f01f944e41e430d7627f2d4af5492639141f6da
Clmra93 <138710421+clmra93@users.noreply.github.com>
2024-09-07 15:24:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,86 @@
+let dados = [
+    {
+        titulo: 'HTML',
+        descricao: 'HTML é a base para a construção de páginas web. É uma linguagem simples e intuitiva, perfeita para iniciantes. Se você quer dar os primeiros passos no desenvolvimento web, este guia te ajudará.',
+        link: 'https://developer.mozilla.org/pt-BR/docs/Web/HTML',
+        tags: "web development frontend markup estrutura base"
+    },
+    {
+        titulo: 'CSS',
+        descricao: 'CSS é usado para estilizar e formatar páginas web. Ele define a aparência visual dos elementos HTML.',
+        link: 'https://developer.mozilla.org/pt-BR/docs/Web/CSS',
+        tags: "css styling stylesheet cascading stylesheets layout"
+    },
+    {
+        titulo: 'JavaScript',
+        descricao: 'JavaScript é uma linguagem de programação que adiciona interatividade a páginas web.',
+        link: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
+        tags: "javascript programming dynamic interactive frontend"
+    },
+    {
+        titulo: 'React',
+        descricao: 'React é uma biblioteca JavaScript para criar interfaces de usuário declarativas e eficientes.',
+        link: 'https://reactjs.org/',
+        tags: "react javascript ui components jsx"
+    },
+    {
+        titulo: 'Angular',
+        descricao: 'Angular é um framework TypeScript para construir aplicações web robustas e escaláveis.',
+        link: 'https://angular.io/',
+        tags: "angular typescript framework components mvc"
+    },
+    {
+        titulo: 'Vue.js',
+        descricao: 'Vue.js é um framework JavaScript progressivo para criar interfaces de usuário.',
+        link: 'https://vuejs.org/',
+        tags: "vue javascript framework components progressive"
+    },
+    {
+        titulo: 'Sass',
+        descricao: 'Sass é um pré-processador CSS que facilita a escrita e manutenção de estilos.',
+        link: 'https://sass-lang.com/',
+        tags: "sass css preprocessor nesting variables"
+    },
+    {
+        titulo: 'Less',
+        descricao: 'Less é um pré-processador CSS que adiciona funcionalidades para estilos CSS.',
+        link: 'https://lesscss.org/',
+        tags: "less css preprocessor mixins variables"
+    },
+    {
+        titulo: 'npm',
+        descricao: 'npm é o gerenciador de pacotes padrão para JavaScript.',
+        link: 'https://www.npmjs.com/',
+        tags: "npm node package manager dependencies registry"
+    },
+    {
+        titulo: 'yarn',
+        descricao: 'yarn é um gerenciador de pacotes rápido e confiável para JavaScript.',
+        link: 'https://yarnpkg.com/',
+        tags: "yarn node package manager fast offline"
+    },
+    {
+        titulo: 'Webpack',
+        descricao: 'Webpack é um empacotador de módulos para JavaScript.',
+        link: 'https://webpack.js.org/',
+        tags: "webpack module bundler javascript modules loaders"
+    },
+    {
+        titulo: 'Parcel',
+        descricao: 'Parcel é um empacotador de módulos rápido e simples para JavaScript.',
+        link: 'https://parceljs.org/',
+        tags: "parcel module bundler fast zero config bundler"
+    },
+    {
+        titulo: 'Jest',
+        descricao: 'Jest é um framework de testes JavaScript.',
+        link: 'https://jestjs.io/',
+        tags: "jest testing javascript snapshot testing mock"
+    },
+    {
+        titulo: 'Mocha',
+        descricao: 'Mocha é um framework de testes flexível para JavaScript.',
+        link: 'https://mochajs.org/',
+        tags: "mocha testing javascript flexible bdd"
+    }
+];
